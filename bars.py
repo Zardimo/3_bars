bfe8565daf98e751956911f70528bb902fd5f589
Zardimo <vasyasapeggin@rambler.ru>
2019-01-21 09:58:53 +0500

    Задача решена

--- bars.py
+++ bars.py
@@ -4,57 +4,56 @@ import json
 
 def load_data(filepath):
     with open(filepath, 'r', encoding='utf-8') as fh:
-        return(json.load(fh))
+        return json.load(fh)
 
 
 def get_biggest_bar(data):
-    list1=[]
+    sorted_size_bar_list=[]
     x = 0
     while x <= (len(data['features'])-1):
-    	с = data['features'][x]['properties']['Attributes']
-    	list1.append(с.copy())
+    	bar_list = data['features'][x]['properties']['Attributes']
+    	sorted_size_bar_list.append(bar_list.copy())
     	x+=1
-    return(max(list1,key= lambda d: d['SeatsCount']).get('Name'))
+    return max(sorted_size_bar_list, key= lambda d: d['SeatsCount']).get('Name')
 
 
 def get_smallest_bar(data):
-    list1=[]
+    sorted_size_bar_list=[]
     x = 0
     while x <= (len(data['features'])-1):
-    	с = data['features'][x]['properties']['Attributes']
-    	list1.append(с.copy())
+    	bar_list = data['features'][x]['properties']['Attributes']
+    	sorted_size_bar_list.append(bar_list.copy())
     	x+=1
-    return(min(list1,key= lambda d: d['SeatsCount']).get('Name'))
+    return min(sorted_size_bar_list, key= lambda d: d['SeatsCount']).get('Name')
 
 
 def get_closest_bar(data, longitude, latitude):
-    list2=[]
+    sorted_close_bar_list=[]
     x = 0
     while x <= (len(data['features'])-1):
-    	d = data['features'][x]['geometry']['coordinates']
-    	list2.append(d.copy())
+    	bars_coordinates = data['features'][x]['geometry']['coordinates']
+    	sorted_close_bar_list.append(bars_coordinates.copy())
     	x+=1
-    e = {}
-    n = []
+    bar_dict = {}
+    sorted_coordinates_list = []
     v = 0
     while v <= (len(data['features'])-1):
-    	n = [(list2[v][0]-longitude)**2+(list2[v][1]-latitude)**2]
-    	c = data['features'][v]['properties']['Attributes']['Name']
-    	e[c] = n
+    	sorted_coordinates_list = [(sorted_close_bar_list[v][0]-longitude)**2+(sorted_close_bar_list[v][1]-latitude)**2]
+    	bar_name = data['features'][v]['properties']['Attributes']['Name']
+    	bar_dict[bar_name] = sorted_coordinates_list
     	v+=1 
-    return(min(e, key=e.get))
+    return min(bar_dict, key=bar_dict.get)
 
 if __name__ == '__main__':
-    h = input("Введите месторасположение файла")
-    g = load_data(h)
+    first_question = input("Введите месторасположение файла")
+    data_bank = load_data(first_question)
     print("Какую информацию по барам Москвы Вы хотите получить?\n1.Вывести самый большой бар Москвы\n2.Вывести самый маленький бар Москвы\n3.Вывести ближайший бар")	
-    u = input("")
-    if u == "1":
-        print(get_biggest_bar(g))	
-    elif u == "2":
-	    print(get_smallest_bar(g))
-    elif u == "3":
+    second_question = input("")
+    if second_question == "1":
+        print(get_biggest_bar(data_bank))	
+    elif second_question == "2":
+	    print(get_smallest_bar(data_bank))
+    elif second_question == "3":
         longitude = float(input("Введите долготу"))
         latitude = float(input("Введите широту"))
-        print(get_closest_bar(g, longitude, latitude))
-    			
+        print(get_closest_bar(data_bank, longitude, latitude))
