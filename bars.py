ada219b33c82dbd4f7f7947c19b63594aab304cb
Zardimo <vasyasapeggin@rambler.ru>
2019-01-31 11:06:14 +0500

    Изменения внесены

--- bars.py
+++ bars.py
@@ -4,33 +4,31 @@ from sys import argv
 
 
 def load_data(filepath):
-    if not os.path.exists(filepath):
-        print("Неверно указан путь файла")
-        raise SystemExit
-    with open(filepath, "r", encoding="utf-8") as info_bars:
-        return json.load(info_bars)
+    if os.path.exists(filepath):
+        with open(filepath, "r", encoding="utf-8") as file_bars:
+            return json.load(file_bars)
 
 
 def get_biggest_bar(bars):
-    return max(bars, key=lambda
+    return max(bars["features"], key=lambda
                d: d["properties"]["Attributes"]["SeatsCount"])
 
 
 def get_smallest_bar(bars):
-    return min(bars, key=lambda
+    return min(bars["features"], key=lambda
                d: d["properties"]["Attributes"]["SeatsCount"])
 
 
 def get_closest_bar(bars):
-    longitude = float(input("Введите долготу: "))
-    latitude = float(input("Введите широту: "))
-    number_name = min(bars, key=lambda
-                      x: coords(x["geometry"]["coordinates"][0], longitude,
-                                x["geometry"]["coordinates"][1], latitude))
+    number_name = min(bars["features"], key=lambda
+                      x: calculate_coords(x["geometry"]["coordinates"][0],
+                      longitude,
+                      x["geometry"]["coordinates"][1],
+                      latitude))
     return number_name
 
 
-def coords(x, x1, y, y1):
+def calculate_coords(x, x1, y, y1):
     delta_x = (x-x1)**2
     delta_y = (y-y1)**2
     return delta_x+delta_y
@@ -38,21 +36,27 @@ def coords(x, x1, y, y1):
 
 if __name__ == "__main__":
     try:
-        bars = load_data(argv[1])["features"]
-        longitude = 0
-        latitude = 0
-        print("Какую информацию по барам Москвы Вы хотите получить?"
-              "\n1.Вывести самый большой бар Москвы"
-              "\n2.Вывести самый маленький бар Москвы"
-              "\n3.Вывести ближайший бар")
-        second_question = int(input('Выберите опцию: '))
-        map = {
-            1: get_biggest_bar,
-            2: get_smallest_bar,
-            3: get_closest_bar
-        }
-        print(map[second_question](bars)["properties"]["Attributes"]["Name"])
+        bars = load_data(argv[1])
     except IndexError:
+        print("Укажите путь к файлу")
+        raise SystemExit
+    if bars is None:
         print("Укажите верный путь к файлу")
+        raise SystemExit
+    option_map = {
+        "1": get_biggest_bar,
+        "2": get_smallest_bar,
+        "3": get_closest_bar
+    }
+    print("Какую информацию по барам Москвы Вы хотите получить?"
+          "\n1.Вывести самый большой бар Москвы"
+          "\n2.Вывести самый маленький бар Москвы"
+          "\n3.Вывести ближайший бар")
+    second_question = input("Выберите опцию: ")
+    try:
+        if second_question == "3":
+            longitude = float(input("Введите долготу: "))
+            latitude = float(input("Введите широту: "))
+        print(option_map[second_question](bars)["properties"]["Attributes"]["Name"])
     except ValueError:
         print("Укажите целые, либо дробные числа")
