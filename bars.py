039a84fd8ca4450cae9659c12c1eba313111a95f
Zardimo <vasyasapeggin@rambler.ru>
2019-02-04 10:03:18 +0500

    Изменения внесены

--- bars.py
+++ bars.py
@@ -7,26 +7,37 @@ from sys import argv
 def load_data(filepath):
     if not os.path.exists(filepath):
         return None
+    fileway, file_format = os.path.splitext(filepath)
+    if file_format != ".json":
+        return None
     with open(filepath, "r", encoding="utf-8") as file_bars:
         return json.load(file_bars)
 
 
 def get_biggest_bar(bars):
-    return max(bars,
-               key=lambda d: d["properties"]["Attributes"]["SeatsCount"])
+    return max(
+        bars,
+        key=lambda d: d["properties"]["Attributes"]["SeatsCount"]
+    )
 
 
 def get_smallest_bar(bars):
-    return min(bars,
-               key=lambda d: d["properties"]["Attributes"]["SeatsCount"])
+    return min(
+        bars,
+        key=lambda d: d["properties"]["Attributes"]["SeatsCount"]
+    )
 
 
-def get_closest_bar(bars):
-    number_name = min(bars, key=lambda
-                      x: calculate_coords(x["geometry"]["coordinates"][0],
-                                          longitude,
-                                          x["geometry"]["coordinates"][1],
-                                          latitude))
+def get_closest_bar(bars, longitude, latitude):
+    number_name = min(
+        bars,
+        key=lambda x: calculate_coords(
+            x["geometry"]["coordinates"][0],
+            longitude,
+            x["geometry"]["coordinates"][1],
+            latitude
+        )
+    )
     return number_name
 
 
@@ -37,22 +48,26 @@ def calculate_coords(x, x1, y, y1):
 
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        bars_dict = load_data(argv[1])
-        if not bars_dict:
-            exit("Укажите верный путь к файлу")
-        bars = bars_dict["features"]
-        print("Самый большой бар: {}".format(
-            get_biggest_bar(bars)["properties"]["Attributes"]["Name"]))
-        print("Самый маленький бар: {}".format(
-            get_smallest_bar(bars)["properties"]["Attributes"]["Name"]))
-        print("Для нахождения ближайшего бара необходимо ввести координаты")
-        try:
-            longitude = float(input("Введите долготу: "))
-            latitude = float(input("Введите широту: "))
-            print("Ближайший бар: {}".format(
-                get_closest_bar(bars)["properties"]["Attributes"]["Name"]))
-        except ValueError:
-            print("Укажите целые, либо дробные числа")
-    else:
-        print("Укажите путь к файлу")
+    if not len(sys.argv) > 1:
+        exit("Укажите путь к файлу")
+    bars_dict = load_data(argv[1])
+    if not bars_dict:
+        exit("Укажите верный путь к файлу")
+    bars = bars_dict["features"]
+    print("Самый большой бар: {}".format(
+        get_biggest_bar(bars)["properties"]["Attributes"]["Name"]))
+    print("Самый маленький бар: {}".format(
+        get_smallest_bar(bars)["properties"]["Attributes"]["Name"]))
+    print("Для нахождения ближайшего бара необходимо ввести координаты")
+    try:
+        longitude = float(input("Введите долготу: "))
+        latitude = float(input("Введите широту: "))
+        print("Ближайший бар: {}".format(
+            get_closest_bar(
+                bars,
+                longitude,
+                latitude)["properties"]["Attributes"]["Name"]
+             )
+        )
+    except ValueError:
+        print("Укажите целые, либо дробные числа")
